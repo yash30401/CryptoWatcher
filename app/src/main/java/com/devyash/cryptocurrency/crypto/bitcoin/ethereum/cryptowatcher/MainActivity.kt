9095e6aedcba55014f608050251081119ec3bdce
yash30401 <yashveersingh30401@gmail.com>
2022-11-21 16:22:44 +0530

    BottomBar Implement

--- app/src/main/java/com/devyash/cryptocurrency/crypto/bitcoin/ethereum/cryptowatcher/MainActivity.kt
+++ app/src/main/java/com/devyash/cryptocurrency/crypto/bitcoin/ethereum/cryptowatcher/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.devyash.cryptocurrency.crypto.bitcoin.ethereum.cryptowatcher.databinding.ActivityMainBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        BottomSheetBehavior.from(bottomsheet).apply {
+        BottomSheetBehavior.from(binding.bottomsheet).apply {
             peekHeight=200
 
             this.state=BottomSheetBehavior.STATE_HALF_EXPANDED
