030137ba45d6b8a26aab7510ab7436ac927c35a8
yash30401 <yashveersingh30401@gmail.com>
2022-11-24 13:26:47 +0530

    Bottombar Swipable.

--- app/src/main/java/com/devyash/cryptocurrency/crypto/bitcoin/ethereum/cryptowatcher/MainActivity.kt
+++ app/src/main/java/com/devyash/cryptocurrency/crypto/bitcoin/ethereum/cryptowatcher/MainActivity.kt
@@ -6,8 +6,14 @@ import android.util.TypedValue
 import android.widget.PopupMenu
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.devyash.cryptocurrency.crypto.bitcoin.ethereum.cryptowatcher.databinding.ActivityMainBinding
+import com.devyash.cryptocurrency.crypto.bitcoin.ethereum.cryptowatcher.fragments.HomeFragment
+import com.devyash.cryptocurrency.crypto.bitcoin.ethereum.cryptowatcher.fragments.MarketFragment
+import com.devyash.cryptocurrency.crypto.bitcoin.ethereum.cryptowatcher.fragments.WatchlistFragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import nl.joery.animatedbottombar.AnimatedBottomBar
 
@@ -23,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         BottomSheetBehavior.from(binding.bottomsheet).apply {
             peekHeight=binding.bottomBar.height+1300
+            this.state=BottomSheetBehavior.STATE_EXPANDED
 
         }
 
@@ -30,14 +37,22 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragment_container_view)
-        val navController=navHostFragment!!.findNavController()
+//        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragment_container_view)
+//        val navController=navHostFragment!!.findNavController()
+//
+//        val popupMenu=PopupMenu(this,null)
+//        popupMenu.inflate(R.menu.bottom_menu)
+//        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
 
-        val popupMenu=PopupMenu(this,null)
-        popupMenu.inflate(R.menu.bottom_menu)
-        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
 
+        val viewPager: ViewPager2=findViewById(R.id.view_pager)
 
+        val fragments:ArrayList<Fragment> = arrayListOf(HomeFragment(),MarketFragment(),WatchlistFragment())
+
+        val adapter=ViewPagerAdapter(fragments,this)
+        viewPager.adapter=adapter
+
+        binding.bottomBar.setupWithViewPager2(viewPager)
 
     }
 }
